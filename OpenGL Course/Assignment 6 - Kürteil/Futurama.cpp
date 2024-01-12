193ed6d6c4200f0935212d10c25279d91b2369d9
Hien Tran <hien.tptran@gmail.com>
2024-01-12 18:10:16 +0100

    Print out node count

--- OpenGL Course/Assignment 6 - Kürteil/Futurama.cpp
+++ OpenGL Course/Assignment 6 - Kürteil/Futurama.cpp
@@ -297,6 +297,7 @@ int main(int argc, char** argv){
   //  build the robot hierarchy (see robot.cpp)
   Node *root= Robot::build();
   std::vector<Node> nodeVector = Robot::nodeVector;
+  cout << "Node count: " << nodeVector.size() << endl;
   
   //make scenegraph
   SceneGraph *sceneGraph= new SceneGraph(root);
